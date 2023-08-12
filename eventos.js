77d4285825c8913a2f7b079e5b1e27ba4b0d4d8f
Romina Gonzalez <rominavgs1996@gmail.com>
2023-08-12 18:18:03 -0400

    Se agrega alerta del div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("myDiv");
+
+btn.style.backgroundColor = "green";
+btn.style.padding = "5px";
+
+btn.addEventListener("click", function () {
+    alert("Hola, soy el div!");
+});
